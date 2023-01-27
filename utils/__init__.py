a975694c4e7766d1a7c74cc9265f93883e2ae983
Jason-Choi <jasonchoi3@g.skku.edu>
2023-01-27 16:41:33 +0000

    [REMOVE] generate prompt function

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,2 @@
-from generate_prompt import generate_prompt
 from upload_images import upload_images
 from utils.get_image import get_image
--- utils/generate_prompt.py
+++
@@ -1,2 +0,0 @@
-def generate_prompt(title, color, desc):
-    return f"title: {title}\ncolor: {color}\nstyle: {desc}"
