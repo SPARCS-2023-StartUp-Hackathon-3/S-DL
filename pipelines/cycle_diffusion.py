0a022ce1878efcc61cd712dfcbdba9fa02a79712
Jason-Choi <jasonchoi3@g.skku.edu>
2023-01-28 05:18:54 +0000

    Revert "[ADD] cross attention control replacement"
    This reverts commit ee613d2f881e6ecbf7e3c1d605080cef818c3753.

--- pipelines/cycle_diffusion.py
+++ pipelines/cycle_diffusion.py
@@ -1,213 +1,21 @@
 import torch
 from diffusers import CycleDiffusionPipeline, DDIMScheduler
 from config import DEVICE_ID, NUM_IMAGES_PER_PROMPT, NUM_INFERENCE_STEPS, MODEL_PATH, DIFFUSION_PATH
+from PIL.Image import Image
 from typing import List
-import torch
-from PIL import Image
-from utils import ptp_utils
-from utils import seq_aligner
-import torch.nn.functional as nnf
-from typing import Optional, Union, Tuple, List, Callable, Dict
-import abc
-
-LOW_RESOURCE = False
-MAX_NUM_WORDS = 77
-device = DEVICE_ID
-torch_dtype = torch.float16
 
 
-scheduler = DDIMScheduler.from_pretrained(
-    DIFFUSION_PATH, subfolder="scheduler")
+scheduler = DDIMScheduler.from_pretrained(DIFFUSION_PATH, subfolder="scheduler")
 pipe = CycleDiffusionPipeline.from_pretrained(
     DIFFUSION_PATH, torch_dtype=torch.float16, scheduler=scheduler
 )
-
-tokenizer = pipe.tokenizer
-
 pipe.unet.load_attn_procs(MODEL_PATH)
 pipe.to(f"cuda:{DEVICE_ID}")
 
 
-class LocalBlend:
 
-    def __call__(self, x_t, attention_store):
-        k = 1
-        maps = attention_store["down_cross"][2:4] + \
-            attention_store["up_cross"][:3]
-        maps = [item.reshape(self.alpha_layers.shape[0], -1,
-                             1, 16, 16, MAX_NUM_WORDS) for item in maps]
-        maps = torch.cat(maps, dim=1)
-        maps = (maps * self.alpha_layers).sum(-1).mean(1)
-        mask = nnf.max_pool2d(maps, (k * 2 + 1, k * 2 + 1),
-                              (1, 1), padding=(k, k))
-        mask = nnf.interpolate(mask, size=(x_t.shape[2:]))
-        mask = mask / mask.max(2, keepdims=True)[0].max(3, keepdims=True)[0]
-        mask = mask.gt(self.threshold)
-        mask = (mask[:1] + mask[1:]).to(x_t.dtype)
-        x_t = x_t[:1] + mask * (x_t - x_t[:1])
-        return x_t
-
-    def __init__(self, prompts: List[str], words, threshold=.3):
-        alpha_layers = torch.zeros(len(prompts),  1, 1, 1, 1, MAX_NUM_WORDS)
-        for i, (prompt, words_) in enumerate(zip(prompts, words)):
-            if type(words_) is str:
-                words_ = [words_]
-            for word in words_:
-                ind = ptp_utils.get_word_inds(prompt, word, tokenizer)
-                alpha_layers[i, :, :, :, :, ind] = 1
-        self.alpha_layers = alpha_layers.to(device).to(torch_dtype)
-        self.threshold = threshold
-
-
-class AttentionControl(abc.ABC):
-
-    def step_callback(self, x_t):
-        return x_t
-
-    def between_steps(self):
-        return
-
-    @property
-    def num_uncond_att_layers(self):
-        return self.num_att_layers if LOW_RESOURCE else 0
-
-    @abc.abstractmethod
-    def forward(self, attn, is_cross: bool, place_in_unet: str):
-        raise NotImplementedError
-
-    def __call__(self, attn, is_cross: bool, place_in_unet: str):
-        if self.cur_att_layer >= self.num_uncond_att_layers:
-            if LOW_RESOURCE:
-                attn = self.forward(attn, is_cross, place_in_unet)
-            else:
-                h = attn.shape[0]
-                attn[h // 2:] = self.forward(attn[h // 2:],
-                                             is_cross, place_in_unet)
-        self.cur_att_layer += 1
-        if self.cur_att_layer == self.num_att_layers + self.num_uncond_att_layers:
-            self.cur_att_layer = 0
-            self.cur_step += 1
-            self.between_steps()
-        return attn
-
-    def reset(self):
-        self.cur_step = 0
-        self.cur_att_layer = 0
-
-    def __init__(self):
-        self.cur_step = 0
-        self.num_att_layers = -1
-        self.cur_att_layer = 0
-
-
-class AttentionStore(AttentionControl):
-    @staticmethod
-    def get_empty_store():
-        return {"down_cross": [], "mid_cross": [], "up_cross": [],
-                "down_self": [],  "mid_self": [],  "up_self": []}
-
-    def forward(self, attn, is_cross: bool, place_in_unet: str):
-        key = f"{place_in_unet}_{'cross' if is_cross else 'self'}"
-        if attn.shape[1] <= 32 ** 2:  # avoid memory overhead
-            self.step_store[key].append(attn)
-        return attn
-
-    def between_steps(self):
-        if len(self.attention_store) == 0:
-            self.attention_store = self.step_store
-        else:
-            for key in self.attention_store:
-                for i in range(len(self.attention_store[key])):
-                    self.attention_store[key][i] += self.step_store[key][i]
-        self.step_store = self.get_empty_store()
-
-    def get_average_attention(self):
-        average_attention = {key: [
-            item / self.cur_step for item in self.attention_store[key]] for key in self.attention_store}
-        return average_attention
-
-    def reset(self):
-        super(AttentionStore, self).reset()
-        self.step_store = self.get_empty_store()
-        self.attention_store = {}
-
-    def __init__(self):
-        super(AttentionStore, self).__init__()
-        self.step_store = self.get_empty_store()
-        self.attention_store = {}
-
-
-class AttentionControlEdit(AttentionStore, abc.ABC):
-
-    def step_callback(self, x_t):
-        if self.local_blend is not None:
-            x_t = self.local_blend(x_t, self.attention_store)
-        return x_t
-
-    def replace_self_attention(self, attn_base, att_replace):
-        if att_replace.shape[2] <= 16 ** 2:
-            return attn_base.unsqueeze(0).expand(att_replace.shape[0], *attn_base.shape)
-        else:
-            return att_replace
-
-    @abc.abstractmethod
-    def replace_cross_attention(self, attn_base, att_replace):
-        raise NotImplementedError
-
-    def forward(self, attn, is_cross: bool, place_in_unet: str):
-        super(AttentionControlEdit, self).forward(
-            attn, is_cross, place_in_unet)
-        if is_cross or (self.num_self_replace[0] <= self.cur_step < self.num_self_replace[1]):
-            h = attn.shape[0] // self.batch_size
-            attn = attn.reshape(self.batch_size, h, *attn.shape[1:])
-            attn_base, attn_repalce = attn[0], attn[1:]
-            if is_cross:
-                alpha_words = self.cross_replace_alpha[self.cur_step]
-                attn_replace_new = self.replace_cross_attention(
-                    attn_base, attn_repalce) * alpha_words + (1 - alpha_words) * attn_repalce
-                attn[1:] = attn_replace_new
-            else:
-                attn[1:] = self.replace_self_attention(attn_base, attn_repalce)
-            attn = attn.reshape(self.batch_size * h, *attn.shape[2:])
-        return attn
-
-    def __init__(self, prompts, num_steps: int,
-                 cross_replace_steps: Union[float, Tuple[float, float], Dict[str, Tuple[float, float]]],
-                 self_replace_steps: Union[float, Tuple[float, float]],
-                 local_blend: Optional[LocalBlend]):
-        super(AttentionControlEdit, self).__init__()
-        self.batch_size = len(prompts)
-        self.cross_replace_alpha = ptp_utils.get_time_words_attention_alpha(
-            prompts, num_steps, cross_replace_steps, tokenizer).to(device).to(torch_dtype)
-        if type(self_replace_steps) is float:
-            self_replace_steps = 0, self_replace_steps
-        self.num_self_replace = int(
-            num_steps * self_replace_steps[0]), int(num_steps * self_replace_steps[1])
-        self.local_blend = local_blend
-
-
-class AttentionReplace(AttentionControlEdit):
-
-    def replace_cross_attention(self, attn_base, att_replace):
-        return torch.einsum('hpw,bwn->bhpn', attn_base, self.mapper)
-
-    def __init__(self, prompts, num_steps: int, cross_replace_steps: float, self_replace_steps: float,
-                 local_blend: Optional[LocalBlend] = None):
-        super(AttentionReplace, self).__init__(prompts, num_steps,
-                                               cross_replace_steps, self_replace_steps, local_blend)
-        self.mapper = seq_aligner.get_replacement_mapper(
-            prompts, tokenizer).to(device).to(torch_dtype)
-
-
-def edit(source_image: Image.Image, source_prompt: str, target_prompt: str) -> List[Image.Image]:
-    controller = AttentionReplace([source_prompt, target_prompt],
-                                  NUM_INFERENCE_STEPS,
-                                  cross_replace_steps=0.8,
-                                  self_replace_steps=0.4,
-                                  )
-    ptp_utils.register_attention_control(pipe, controller)
-    
-    images: List[Image.Image] = pipe(
+def edit(source_image: Image, source_prompt: str, target_prompt: str) -> List[Image]:
+    images: List[Image] = pipe(
         prompt=target_prompt,
         source_prompt=source_prompt,
         image=source_image,
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,2 @@
 from utils.upload_images import upload_images
 from utils.get_image import get_image
-from utils.ptp_utils import *
-from utils.seq_aligner import *
