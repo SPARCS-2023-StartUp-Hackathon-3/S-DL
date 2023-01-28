4a79bf7db71c82ad2426294826e1efdc6cfb50c7
Jason-Choi <jasonchoi3@g.skku.edu>
2023-01-28 05:15:21 +0000

    [MOD] train script

--- train.sh
+++ train.sh
@@ -1,14 +1,12 @@
-export MODEL_NAME="stabilityai/stable-diffusion-2-1-base"
-
 accelerate launch train_lora.py \
-  --pretrained_model_name_or_path=$MODEL_NAME \
+  --pretrained_model_name_or_path="stabilityai/stable-diffusion-2-1-base" \
   --dataset_name="jasonchoi3/nordstrom96568" --caption_column="text" \
   --resolution=512 --random_flip \
-  --train_batch_size=4 \
-  --max_train_steps=25000 --checkpointing_steps=5000 \
-  --learning_rate=1e-4 --lr_scheduler="constant" --lr_warmup_steps=0 \
+  --train_batch_size=16 \
+  --max_train_steps=20000 --checkpointing_steps=5000 \
+  --learning_rate=5e-5 --lr_scheduler="constant" --lr_warmup_steps=0 \
   --seed=42 \
-  --output_dir="outputs" --mixed_precision="fp16" \
+  --output_dir="models" --mixed_precision="fp16" \
   --report_to="wandb" \
   --local_rank=0 \
   --resume_from_checkpoint="latest" --validation_prompt="A pair of black pants"
